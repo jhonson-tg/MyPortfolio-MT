0c0e77de372d6385cba395b0c85faffbb5d6818b
jhonson.tg <jhonsontg@gmail.com>
2023-03-25 21:01:48 +0530

    initial commit

---
+++ src/main/java/com/mrj33/myportfoliomt/MyportfolioMtApplication.java
@@ -0,0 +1,13 @@
+package com.mrj33.myportfoliomt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyportfolioMtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyportfolioMtApplication.class, args);
+	}
+
+}
